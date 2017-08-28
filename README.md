a0ee4fc9c41d42616929c47cd1ae6aece6839be2
sjtutyx <sjtutyx@gmail.com>
2017-08-28 16:08:19 -0700

    add README.md

---
+++ README.md
@@ -0,0 +1,104 @@
+## SDUWN
+
+#### Environment Configuration
+1.Disable IPv6 for preventing TAP devices/bridges broadcasting routing:
+
+- **Step 1:** Edit `/etc/sysctl.conf` (with sudo) and open the configuration file and add the following lines at the end:
+
+```
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1
+```
+
+- **Step 2:** After that run `$ cat /proc/sys/net/ipv6/conf/all/disable_ipv6`
+If it reports ‘1′ means you have disabled IPV6. 
+If it reports ‘0‘ then please follow Step 3.
+
+- **Step 3:** Type command `sudo sysctl -p` you will see this in terminal.
+```
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1
+```
+
+2.Compile Mininet
+
+- **Step 1:** Move the `opennet-agent.py` into `/usr/bin/`:
+```
+sudo chmod 777 opennet-agent.py
+sudo mv opennet-agent.py /usr/bin/
+```
+- **Step 2:** Install Mininet
+```
+./mininet/util/install.sh -a
+```
+You might need to delete `pox/` and `openflow/` if errors like "pox/openflow exists!" occur.
+
+- **Step 3:** Go into the mininet subdirectory:
+```
+cd mininet/
+sudo python setup.py install
+```
+3.Compile NS-3
+
+- **Step 1:** Some preparations
+```
+sudo sh prepinstall.sh
+```
+- **Step 2:** Configuration 
+```
+sudo CXXFLAGS="-std=c++0x" ./waf configure --enable-sudo
+```
+- **Step 3:** Build 
+```
+sudo ./waf build
+sudo ./waf --apiscan=all
+sudo ./waf install
+```
+4.Test
+- **Step 1:** Open one terminal(named as T1), in T1 run:
+```
+sudo sh pre-config.sh
+```
+- **Step 2:** Open another terminal(named as T2), in T2 run:
+```
+cd examples/
+sudo python one-switch-in-band-controller-csma.py
+```
+- **Step 3:** in T1 run:
+```
+sudo sh post-config.sh
+```
+
+If there is no error, then everything should be done.
+
+
+####Exceptions:
+**Error:**
+ImportError: libns3.25-core-debug.so: cannot open shared object file: No such file or directory
+
+**Solution:**
+
+- **Step 1:** 
+```
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/sduwn/ns-allinone-3.25/ns-3.25/build
+```
+- **Step 2:**
+```
+sudo ldconfig
+```
+
+
+**Error:**
+RTNETLINK answers: File exists
+
+**Solution:**
+
+- **Step1:** Before test, run:
+```
+sudo mn -c
+```
+
+Yuxin Tang
+08/27/2017
---
+++ opennet-agent.py
@@ -0,0 +1,193 @@
+#!/usr/bin/python
+import sys, os, time, atexit
+from signal import SIGTERM
+
+import socket
+import multiprocessing
+
+from re import findall
+from threading import Thread
+from time import sleep
+
+from datetime import datetime
+
+import mininet.node
+import mininet.link
+
+from ns.lte import *
+from ns.core import *
+from ns.network import *
+from ns.internet import *
+from ns.mobility import *
+from ns.fd_net_device import *
+from ns.tap_bridge import *
+from ns.wifi import *
+
+class Daemon:
+    """
+    A generic daemon class.
+
+    Usage: subclass the Daemon class and override the run() method
+    """
+    def __init__(self, pidfile, stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
+        self.stdin = stdin
+        self.stdout = stdout
+        self.stderr = stderr
+        self.pidfile = pidfile
+
+    def daemonize(self):
+        """
+        do the UNIX double-fork magic, see Stevens' "Advanced
+        Programming in the UNIX Environment" for details (ISBN 0201563177)
+        http://www.erlenstar.demon.co.uk/unix/faq_2.html#SEC16
+        """
+        try:
+            pid = os.fork()
+            if pid > 0:
+                # exit first parent
+                sys.exit(0)
+        except OSError, e:
+            sys.stderr.write("fork #1 failed: %d (%s)\n" % (e.errno, e.strerror))
+            sys.exit(1)
+
+        # decouple from parent environment
+        os.chdir("/")
+        os.setsid()
+        os.umask(0)
+
+        # do second fork
+        try:
+            pid = os.fork()
+            if pid > 0:
+                # exit from second parent
+                sys.exit(0)
+        except OSError, e:
+            sys.stderr.write("fork #2 failed: %d (%s)\n" % (e.errno, e.strerror))
+            sys.exit(1)
+
+        # redirect standard file descriptors
+        sys.stdout.flush()
+        sys.stderr.flush()
+        si = file(self.stdin, 'r')
+        so = file(self.stdout, 'a+')
+        se = file(self.stderr, 'a+', 0)
+        os.dup2(si.fileno(), sys.stdin.fileno())
+        os.dup2(so.fileno(), sys.stdout.fileno())
+        os.dup2(se.fileno(), sys.stderr.fileno())
+
+        # write pidfile
+        atexit.register(self.delpid)
+        pid = str(os.getpid())
+        file(self.pidfile,'w+').write("%s\n" % pid)
+
+    def delpid(self):
+        os.remove(self.pidfile)
+
+    def start(self):
+        """
+        Start the daemon
+        """
+        # Check for a pidfile to see if the daemon already runs
+        try:
+            pf = file(self.pidfile,'r')
+            pid = int(pf.read().strip())
+            pf.close()
+        except IOError:
+            pid = None
+
+        if pid:
+            message = "pidfile %s already exist. Daemon already running?\n"
+            sys.stderr.write(message % self.pidfile)
+            sys.exit(1)
+
+        # Start the daemon
+        self.daemonize()
+        self.run()
+
+    def stop(self):
+        """
+        Stop the daemon
+        """
+        # Get the pid from the pidfile
+        try:
+            pf = file(self.pidfile,'r')
+            pid = int(pf.read().strip())
+            pf.close()
+        except IOError:
+            pid = None
+
+        if not pid:
+            message = "pidfile %s does not exist. Daemon not running?\n"
+            sys.stderr.write(message % self.pidfile)
+            return # not an error in a restart
+
+        # Try killing the daemon process       
+        try:
+            while 1:
+                os.kill(pid, SIGTERM)
+                time.sleep(0.1)
+        except OSError, err:
+            err = str(err)
+            if err.find("No such process") > 0:
+                if os.path.exists(self.pidfile):
+                    os.remove(self.pidfile)
+            else:
+                print str(err)
+                sys.exit(1)
+
+    def restart(self):
+        """
+        Restart the daemon
+        """
+        self.stop()
+        self.start()
+
+    def run(self):
+        """
+        You should override this method when you subclass Daemon. It will be called after the process has been
+        daemonized by start() or restart().
+        """
+        pass
+
+class OpenNetAgent(Daemon):
+    def run (self):
+        msock = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+        msock.setsockopt (socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        msock.bind (('', 53724))
+        msock.listen (64)
+        while True:
+            csock, caddr = msock.accept ()
+            p = multiprocessing.Process (target=self.clientHandler, args=(csock,))
+            p.start ()
+
+    def clientHandler (self, csock):
+        sys.stdout = open ('/tmp/opennet-agent.out', 'a')
+        sys.stderr = open ('/tmp/opennet-agent.err', 'a')
+        while True:
+            data = csock.recv (8192)
+            print str (datetime.now ())
+            print data
+            if data == "exit":
+                break
+            cmd = compile (data, '<string>', 'exec')
+            exec cmd
+            sys.stdout.flush ()
+            sys.stderr.flush ()
+        csock.close ()
+
+if __name__ == "__main__":
+    daemon = OpenNetAgent('/tmp/opennet-agent.pid')
+    if len(sys.argv) == 2:
+        if 'start' == sys.argv[1]:
+            daemon.start()
+        elif 'stop' == sys.argv[1]:
+            daemon.stop()
+        elif 'restart' == sys.argv[1]:
+            daemon.restart()
+        else:
+            print "Unknown command"
+            sys.exit(2)
+        sys.exit(0)
+    else:
+        print "usage: %s start|stop|restart" % sys.argv[0]
+        sys.exit(2)
