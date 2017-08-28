47fb24eb302a9a1d8d407339ace35eaca5499407
sjtutyx <sjtutyx@gmail.com>
2017-08-28 12:25:52 -0700

    Remove the now ignored directory mininet, pox, oftest, openflow, oflops

--- openvswitch-2.7.2/.travis/linux-prepare.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git clone git://git.kernel.org/pub/scm/devel/sparse/chrisl/sparse.git
-cd sparse && make && make install && cd ..
-
-pip install --disable-pip-version-check --user six flake8 hacking
--- openvswitch-2.7.2/datapath/linux/.tmp_versions/vport-vxlan.mod
+++
@@ -1,2 +0,0 @@
-/home/yuxin/Workspace/SDUWN/openvswitch-2.7.2/datapath/linux/vport-vxlan.ko
-/home/yuxin/Workspace/SDUWN/openvswitch-2.7.2/datapath/linux/vport-vxlan.o
